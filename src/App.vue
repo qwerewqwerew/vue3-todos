a228d243f2d533919bb837a1854b193a95fe97da
qwerewqwerew <qwerew0@gmail.com>
2023-03-19 20:30:42 +0900

    02

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
 	<div class="hi">안녕하세요{{ name }}</div>
+	<div class="hi">안녕하세요{{ name }}</div>
+	<div class="hi">안녕하세요{{ name }}</div>
+	<div class="hi">안녕하세요{{ name }}</div>
+	<h1>{{ greeting("Vue") }}</h1>
 </template>
 
 <script>
 	export default {
 		setup() {
 			const name = "Manao";
+			const greeting = (name) => {
+				return "정말쉽다" + name;
+			};
 			return {
 				name,
+				greeting,
 			};
 		},
 	};
