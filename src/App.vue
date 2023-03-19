ac650b03c66947f7e3a3c90c284f17dd8cd6ec56
qwerewqwerew <qwerew0@gmail.com>
2023-03-19 20:09:40 +0900

    02

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="hi">안녕하세요{{ name }}</div>
+</template>
+
+<script>
+	export default {
+		setup() {
+			const name = "Manao";
+			return {
+				name,
+			};
+		},
+	};
+</script>
+
+<style>
+	body {
+		background: yellow;
+	}
+	.hi {
+		color: green;
+	}
+</style>
