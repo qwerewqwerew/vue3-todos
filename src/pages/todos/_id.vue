22af03ed7f046ec39c04cafa8c163b3ac17f2bbe
qwerewqwerew <qwerew0@gmail.com>
2023-04-09 17:25:54 +0900

    10-01

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>일정목록</h1>
+</template>
+
+
+<script>
+import { useRoute } from "vue-router";
+
+export default {
+  setup() {
+    const route = useRoute();
+    console.log(route.params.id);
+  },
+};
+</script>
+
+<style>
+</style>
