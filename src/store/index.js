59adb57d8f79a7c35622d84813fa6007638c8b24
qwerewqwerew <qwerew0@gmail.com>
2023-04-23 16:09:41 +0900

    15-2

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ export default createStore({
         toastMessage: '',
         toastAlertType: '',
         showToast: false,
-        timeout: null
     },
     mutations: {
         UPDATE_TOAST_MESSAGE(state, payload) {
@@ -26,12 +25,11 @@ export default createStore({
             commit('UPDATE_TOAST_MESSAGE', message)
             commit('UPDATE_TOAST_ALERT_TYPE', type)
             commit('UPDATE_TOAST_STATUS', true)
-            const timeout = setTimeout(() => {
+           setTimeout(() => {
                 commit('UPDATE_TOAST_MESSAGE', '')
                 commit('UPDATE_TOAST_ALERT_TYPE', '')
                 commit('UPDATE_TOAST_STATUS', false)
             }, 3000)
-            commit('UPDATE_TOAST_TIMEOUT', timeout);
         }
     },
     getters: {
